70cbf9969c5bc853860ac27b28c0710444bfa9f4
masonjones103 <117707387+masonjones103@users.noreply.github.com>
2025-06-13 19:42:04 -0700

    v0.1

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ port = 4000
 database1 = 'zipcodes_one'
 database2 = 'zipcodes_two'
 
-def get_connection(database):
+def get_connection1():
     return create_engine(
         url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
-            user, password, host, port, database
+            user, password, host, port, database1
+        )
+    )
+
+def get_connection2():
+    return create_engine(
+        url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
+            user, password, host, port, database2
         )
     )
 
@@ -19,11 +26,11 @@ if __name__ == '__main__':
     try:
       
         # GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
-        engine = get_connection(database1)
+        engine = get_connection1()
         print(
-            f"Connection to {host}, database: {database}, for user {user} created successfully.")
-        engine = get_connection(database2)
+            f"Connection to {host}, database: {database1}, for user {user} created successfully.")
+        engine = get_connection2()
         print(
-            f"Connection to {host}, database: {database}, for user {user} created successfully.")
+            f"Connection to {host}, database: {database2}, for user {user} created successfully.")
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
